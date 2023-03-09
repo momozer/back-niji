c413d281fe4d686dce4687ad39334cc364391292
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-09 17:32:04 +0100

    feat : Idee entity develoment

--- src/main/java/com/niji/lille/nijiVerse/entities/Idee.java
+++ src/main/java/com/niji/lille/nijiVerse/entities/Idee.java
@@ -1,4 +1,28 @@
 package com.niji.lille.nijiVerse.entities;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "idee")
 public class Idee extends AbstractEntity{
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "vote")
+    private Integer vote;
+
+    @Column(name = "titre")
+    private String titre;
+
+    @Column(name = "description")
+    private String description;
 }
