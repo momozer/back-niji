14c902e8ae887e92e6f120318252e9f6e310dd23
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-10 15:12:09 +0100

    feat : PlaceService development

--- src/main/java/com/niji/lille/nijiVerse/controllers/PlaceController.java
+++ src/main/java/com/niji/lille/nijiVerse/controllers/PlaceController.java
@@ -1,4 +1,18 @@
 package com.niji.lille.nijiVerse.controllers;
 
+import com.niji.lille.nijiVerse.services.PlaceService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/places")
+@CrossOrigin
 public class PlaceController {
+
+    private final PlaceService service;
+
+    public PlaceController(PlaceService service) {
+        this.service = service;
+    }
 }
