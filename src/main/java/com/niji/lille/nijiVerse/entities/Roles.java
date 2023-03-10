aa57b11257aa0eb536668aa0a66a0444e28c809e
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-10 10:21:31 +0100

    feat : Roles entity development

--- src/main/java/com/niji/lille/nijiVerse/entities/Roles.java
+++ src/main/java/com/niji/lille/nijiVerse/entities/Roles.java
@@ -1,5 +1,6 @@
 package com.niji.lille.nijiVerse.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -15,5 +16,69 @@ import lombok.NoArgsConstructor;
 public class Roles extends AbstractEntity{
 
     @Id
+    @Column(name = "id")
     private String id;
+
+    @Column(name = "directeur")
+    private String directeur;
+
+    @Column(name = "ressourcesHumaines")
+    private String ressourcesHumaines;
+
+    @Column(name = "manageur")
+    private String manageur;
+
+    @Column(name = "collaborateur")
+    private String collaborateur;
+
+    @Column(name = "stagiaire")
+    private String stagiaire;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDirecteur() {
+        return directeur;
+    }
+
+    public void setDirecteur(String directeur) {
+        this.directeur = directeur;
+    }
+
+    public String getRessourcesHumaines() {
+        return ressourcesHumaines;
+    }
+
+    public void setRessourcesHumaines(String ressourcesHumaines) {
+        this.ressourcesHumaines = ressourcesHumaines;
+    }
+
+    public String getManageur() {
+        return manageur;
+    }
+
+    public void setManageur(String manageur) {
+        this.manageur = manageur;
+    }
+
+    public String getCollaborateur() {
+        return collaborateur;
+    }
+
+    public void setCollaborateur(String collaborateur) {
+        this.collaborateur = collaborateur;
+    }
+
+    public String getStagiaire() {
+        return stagiaire;
+    }
+
+    public void setStagiaire(String stagiaire) {
+        this.stagiaire = stagiaire;
+    }
 }
