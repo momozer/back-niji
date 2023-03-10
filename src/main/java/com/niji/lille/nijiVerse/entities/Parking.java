a40820837bbe7e35768a59d4825e06c7ca7bc2e5
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-10 10:02:16 +0100

    feat : Parking and User entities developement

--- src/main/java/com/niji/lille/nijiVerse/entities/Parking.java
+++ src/main/java/com/niji/lille/nijiVerse/entities/Parking.java
@@ -1,4 +1,61 @@
 package com.niji.lille.nijiVerse.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "parking")
 public class Parking extends AbstractEntity{
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "nom")
+    private String nom;
+
+    @Column(name = "placeLibre")
+    private Integer placeLibre;
+
+    @Column(name = "placeMax")
+    private Integer placeMax = 6;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Integer getPlaceLibre() {
+        return placeLibre;
+    }
+
+    public void setPlaceLibre(Integer placeLibre) {
+        this.placeLibre = placeLibre;
+    }
+
+    public Integer getPlaceMax() {
+        return placeMax;
+    }
+
+    public void setPlaceMax(Integer placeMax) {
+        this.placeMax = placeMax;
+    }
 }
--- src/main/java/com/niji/lille/nijiVerse/entities/Place.java
+++ src/main/java/com/niji/lille/nijiVerse/entities/Place.java
@@ -1,4 +1,53 @@
 package com.niji.lille.nijiVerse.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.lang.annotation.Documented;
+
+@Data
+@Table(name = "place")
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Place extends AbstractEntity{
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "nom")
+    private String nom;
+
+    @Column(name = "occupe")
+    private boolean occupe;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public boolean isOccupe() {
+        return occupe;
+    }
+
+    public void setOccupe(boolean occupe) {
+        this.occupe = occupe;
+    }
 }
