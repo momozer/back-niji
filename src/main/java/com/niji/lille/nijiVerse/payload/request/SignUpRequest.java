c9ab7254202deb66015466c4bfb3fb91de3e04fe
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-22 10:13:13 +0100

    feat : SignUpRequest class development

---
+++ src/main/java/com/niji/lille/nijiVerse/payload/request/SignUpRequest.java
@@ -0,0 +1,45 @@
+package com.niji.lille.nijiVerse.payload.request;
+import java.util.Set;
+
+public class SignUpRequest {
+
+    private String username;
+
+    private String email;
+
+    private String motPasse;
+
+    private Set<String> role;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<String> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMotPasse() {
+        return motPasse;
+    }
+
+    public void setMotPasse(String motPasse) {
+        this.motPasse = motPasse;
+    }
+}
