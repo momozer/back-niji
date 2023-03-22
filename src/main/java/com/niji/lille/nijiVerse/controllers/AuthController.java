da0bec0cb93262e5c1529f5689ad18ff3b5b34f9
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-22 10:39:28 +0100

    feat: creation of the AuthController class to secure the application

---
+++ src/main/java/com/niji/lille/nijiVerse/controllers/AuthController.java
@@ -0,0 +1,13 @@
+package com.niji.lille.nijiVerse.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+
+}
