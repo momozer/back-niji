16c4fdfbd61b33e450107aa2166c3b4a0b10cb34
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-23 08:25:00 +0100

    feat : method in ApplicationConfig class added

--- src/main/java/com/niji/lille/nijiVerse/repositories/UserRepository.java
+++ src/main/java/com/niji/lille/nijiVerse/repositories/UserRepository.java
@@ -2,8 +2,6 @@ package com.niji.lille.nijiVerse.repositories;
 
 import com.niji.lille.nijiVerse.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
