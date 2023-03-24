d9085b5bc2da0346cbabc0a0f2f94a4f641c0e78
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-24 16:58:12 +0100

    doc: SecurityConfiguration class documented

--- src/main/java/com/niji/lille/nijiVerse/security/config/SecurityConfiguration.java
+++ src/main/java/com/niji/lille/nijiVerse/security/config/SecurityConfiguration.java
@@ -17,10 +17,37 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
+    /*
+     * Ce code configure la sécurité de l'application web en utilisant Spring Security.
+     * Il crée une chaîne de filtres de sécurité pour gérer les requêtes entrantes et sortantes.
+     */
+
+    /**
+     * un filtre qui extrait le jeton JWT de l'en-tête de demande, valide le jeton et configure l'authentification
+     * en fonction des informations du jeton.
+     */
     private final JwtAuthenticationFilter jwtAuthFilter;
+
+    /**
+     * fournit l'authentification de l'utilisateur en fonction des informations fournies.
+     */
     private final AuthenticationProvider authenticationProvider;
+
+    /**
+     * fournit une gestion personnalisée du processus de déconnexion
+     */
     private final LogoutHandler logoutHandler;
 
+    /**
+     * Le filtre de sécurité est configuré pour désactiver la protection CSRF, autoriser les requêtes vers /auth/** sans
+     * authentification, exiger une authentification pour toutes les autres requêtes, utiliser une politique de session
+     * sans état pour les sessions utilisateur et enfin, ajouter un filtre de filtre
+     * personnalisé (JwtAuthenticationFilter) pour toutes les requêtes entrantes avant le filtre
+     * de nom d'utilisateur/mot de passe (UsernamePasswordAuthenticationFilter).
+     * @param http protocole de requete
+     * @return  le build de Http
+     * @throws Exception
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
