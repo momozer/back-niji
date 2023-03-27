68a5e062c7eeedcd17d9921e3df6a9b05de49f11
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-27 08:46:57 +0200

    doc: RegisterRequest documented

--- src/main/java/com/niji/lille/nijiVerse/security/auth/RegisterRequest.java
+++ src/main/java/com/niji/lille/nijiVerse/security/auth/RegisterRequest.java
@@ -12,6 +12,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    /**
+     *Ce code définit une classe Java nommée "RegisterRequest" utilisée pour encapsuler les données
+     * d'une demande d'inscription. La classe possède les champs suivants:
+     *
+     * "nom": de type String, représentant le nom de l'utilisateur.
+     * "username": de type String, représentant le nom d'utilisateur choisi par l'utilisateur.
+     * "email": de type String, représentant l'adresse e-mail de l'utilisateur.
+     * "motPasse": de type String, représentant le mot de passe de l'utilisateur.
+     */
     private String nom;
     private String username;
     private String email;
