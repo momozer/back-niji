ef83a4aefb75716a07507c24e0845fd2878c75bd
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-27 16:55:51 +0200

    feat : annotations kafka and configuration added

--- src/main/java/com/niji/lille/nijiVerse/parkingKafka/KafkaParkingConfig.java
+++ src/main/java/com/niji/lille/nijiVerse/parkingKafka/KafkaParkingConfig.java
@@ -1,4 +1,7 @@
 package com.niji.lille.nijiVerse.parkingKafka;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class KafkaParkingConfig {
 }
--- src/main/java/com/niji/lille/nijiVerse/parkingKafka/KafkaParkingConsumer.java
+++ src/main/java/com/niji/lille/nijiVerse/parkingKafka/KafkaParkingConsumer.java
@@ -1,4 +1,9 @@
 package com.niji.lille.nijiVerse.parkingKafka;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@Configuration
 public class KafkaParkingConsumer {
 }
