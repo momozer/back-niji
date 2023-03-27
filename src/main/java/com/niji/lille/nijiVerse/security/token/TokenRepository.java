d4c8e1d10fbd3bc57922f1e2832a3d1841bb1750
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-27 09:12:32 +0200

    doc : TokenRepository documented

--- src/main/java/com/niji/lille/nijiVerse/security/token/TokenRepository.java
+++ src/main/java/com/niji/lille/nijiVerse/security/token/TokenRepository.java
@@ -7,6 +7,14 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TokenRepository  extends JpaRepository<Token, Long> {
+
+    /**
+     * La méthode findAllValidTokenByUser est une requête personnalisée (écrite en JPQL)
+     * qui permet de récupérer tous les jetons (tokens) valides pour un utilisateur spécifique.
+     * Elle prend en paramètre l'ID de l'utilisateur et retourne une liste de Token.
+     * @param id de l'utilisateur
+     * @return une liste de Token
+     */
     @Query(value = """
       select t from Token t inner join User u\s
       on t.user.id = u.id\s
@@ -14,5 +22,12 @@ public interface TokenRepository  extends JpaRepository<Token, Long> {
       """)
     List<Token> findAllValidTokenByUser(Long id);
 
+    /**
+     * La méthode findByToken permet de récupérer un objet Token à partir de son jeton (token) unique.
+     * Elle prend en paramètre le jeton (token) en question et retourne un objet Optional<Token> pour prendre en compte
+     * le cas où le jeton n'existe pas dans la base de données.
+     * @param token le jeton (token) en question
+     * @return un objet Optional<Token>
+     */
     Optional<Token> findByToken(String token);
 }
