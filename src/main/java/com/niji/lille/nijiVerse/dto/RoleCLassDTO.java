4ae22b932a59ef4e21a0850ab788316f4b26abb5
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-31 14:39:57 +0200

    feat : added code in RoleClassDTO

--- src/main/java/com/niji/lille/nijiVerse/dto/RoleCLassDTO.java
+++ src/main/java/com/niji/lille/nijiVerse/dto/RoleCLassDTO.java
@@ -1,4 +1,29 @@
 package com.niji.lille.nijiVerse.dto;
 
+import com.niji.lille.nijiVerse.entities.RoleClass;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Builder @Data
 public class RoleCLassDTO {
+    private Long id;
+    private String name;
+
+    public static RoleCLassDTO fromEntity(RoleClass roleClass){
+        if (roleClass == null){
+            return null;
+        }
+        return RoleCLassDTO.builder().build();
+    }
+
+    public static RoleClass toEntity(RoleCLassDTO roleCLassDTO){
+        if (roleCLassDTO == null){
+            return null;
+        }
+        RoleClass roleClass = new RoleClass();
+        roleClass.setName(roleCLassDTO.getName());
+        return roleClass;
+    }
 }
