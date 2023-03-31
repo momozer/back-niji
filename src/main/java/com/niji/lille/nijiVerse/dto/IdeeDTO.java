59a90fffbf144095af27f5cd67edda808f68e34b
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-03-31 14:09:57 +0200

    feat : added developement in ideeDTO class

--- src/main/java/com/niji/lille/nijiVerse/dto/IdeeDTO.java
+++ src/main/java/com/niji/lille/nijiVerse/dto/IdeeDTO.java
@@ -1,4 +1,34 @@
 package com.niji.lille.nijiVerse.dto;
 
+import com.niji.lille.nijiVerse.entities.Idee;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Builder @Data
 public class IdeeDTO {
+    private Long id;
+    private Integer vote;
+    private String titre;
+    private String description;
+
+    public static IdeeDTO fromEntity(Idee idee){
+        if (idee == null){
+            return null;
+        }
+        return IdeeDTO.builder().build();
+    }
+
+    public static Idee toEntity(IdeeDTO ideeDTO){
+        if (ideeDTO == null){
+            return null;
+        }
+        Idee idee = new Idee();
+        idee.setTitre(ideeDTO.getTitre());
+        idee.setDescription(ideeDTO.getDescription());
+        idee.setVote(ideeDTO.getVote());
+
+        return idee;
+    }
 }
