4eebc3eab77c78bf9290fb943b726606f71d3908
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-04-05 16:55:02 +0200

    fix : problem spaces in user class

--- src/main/java/com/niji/lille/nijiVerse/entities/User.java
+++ src/main/java/com/niji/lille/nijiVerse/entities/User.java
@@ -46,7 +46,7 @@ public class User implements UserDetails {
 
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles",
-            joinColumns =  = @JoinColumn(name = "user_id"),
+            joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<Role> roles = new HashSet<>();
