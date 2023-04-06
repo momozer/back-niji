5556220e1e6d649346ff7bdd53f075a10cbee268
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-04-06 15:39:54 +0200

    feat : creation of payload for loginRequest and SignupRequest

---
+++ src/main/java/com/niji/lille/nijiVerse/payload/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.niji.lille.nijiVerse.payload.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String motPasse;
+}
