7d5cff688265721a42da129369e95d5f6cc6de0c
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-04-06 12:01:38 +0200

    feat : developement of AuthTokenFilter class

---
+++ src/main/java/com/niji/lille/nijiVerse/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,25 @@
+package com.niji.lille.nijiVerse.security.jwt;
+
+import com.niji.lille.nijiVerse.security.servicesSecu.UserDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    private JwtUtils jwtUtils;
+    private UserDetailsServiceImpl userDetailsService;
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
