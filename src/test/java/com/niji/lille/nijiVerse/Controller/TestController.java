e91bc3a349338daf3ab01087ab6b10301430eccb
Mohamed Zerouali <zerouali.mi@gmail.com>
2023-04-17 10:22:36 +0200

    fix : fixed files test

--- src/test/java/com/niji/lille/nijiVerse/Controller/TestController.java
+++ src/test/java/com/niji/lille/nijiVerse/Controller/TestController.java
@@ -1,4 +1,4 @@
-package com.niji.lille.nijiVerse.controllers;
+package com.niji.lille.nijiVerse.Controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
